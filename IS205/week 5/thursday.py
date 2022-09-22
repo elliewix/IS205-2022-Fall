08358e955ee283584e3b7fbeb258dec971b1a791
Elizabeth Wickes <elliewix@gmail.com>
2022-09-22 10:16:41 -0500

    from class today

---
+++ IS205/week 5/thursday.py
@@ -0,0 +1,28 @@
+# import random
+# options = ["cat", "dog", "snake"]
+# print(random.choice(options))
+
+# import random as rando
+# options = ["cat", "dog", "snake"]
+# print(rando.choice(options))
+
+# from random import choice
+# options = ["cat", "dog", "snake"]
+# print(choice(options))
+
+# from random import *
+# options = ["cat", "dog", "snake"]
+# print(choice(options))
+x = 1
+# from random import choice
+# import random
+# from random import *
+# import numpy as np
+# from numpy import *
+# from math import *
+import numpy as np
+import math
+print(math.sqrt(9))
+print(np.sqrt(9))
+print(dir())
+
